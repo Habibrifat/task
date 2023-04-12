afee29579222a04ca79699dc3ec9581cb243d7af
Habibrifat <shah.newaz.habib.rifat@gmail.com>
2023-04-12 12:16:40 +0600

    fixed card popup

---
+++ src/popup.js
@@ -0,0 +1,186 @@
+// const showPopupButton = document.querySelector("#showPopupButton");
+const showPopupButton1 = document.querySelector("#showPopupButton1");
+const showPopupButton2 = document.querySelector("#showPopupButton2");
+const showPopupButton3 = document.querySelector("#showPopupButton3");
+const showPopupButton4 = document.querySelector("#showPopupButton4");
+const showPopupButton5 = document.querySelector("#showPopupButton5");
+const showPopupButton6 = document.querySelector("#showPopupButton6");
+const showPopupButton7 = document.querySelector("#showPopupButton7");
+const showPopupButton8 = document.querySelector("#showPopupButton8");
+const showPopupButton9 = document.querySelector("#showPopupButton9");
+const showPopupButton10 = document.querySelector("#showPopupButton10");
+const showPopupButton11 = document.querySelector("#showPopupButton11");
+const showPopupButton12 = document.querySelector("#showPopupButton12");
+const showPopupButton13 = document.querySelector("#showPopupButton13");
+const showPopupButton14 = document.querySelector("#showPopupButton14");
+const showPopupButton15 = document.querySelector("#showPopupButton15");
+const showPopupButton16 = document.querySelector("#showPopupButton16");
+// const showPopupButton3 = document.querySelector("#showPopupButton14");
+const closeButton1 = document.querySelector("#closeButton1");
+const closeButton2 = document.querySelector("#closeButton2");
+const closeButton3 = document.querySelector("#closeButton3");
+const closeButton4 = document.querySelector("#closeButton4");
+const closeButton5 = document.querySelector("#closeButton5");
+const closeButton6 = document.querySelector("#closeButton6");
+const closeButton7 = document.querySelector("#closeButton7");
+const closeButton8 = document.querySelector("#closeButton8");
+const closeButton9 = document.querySelector("#closeButton9");
+const closeButton10 = document.querySelector("#closeButton10");
+const closeButton11 = document.querySelector("#closeButton11");
+const closeButton12 = document.querySelector("#closeButton12");
+const closeButton13 = document.querySelector("#closeButton13");
+const closeButton14 = document.querySelector("#closeButton14");
+const closeButton15 = document.querySelector("#closeButton15");
+const closeButton16 = document.querySelector("#closeButton16");
+
+const popup1 = document.querySelector("#popup1");
+const popup2 = document.querySelector("#popup2");
+const popup3 = document.querySelector("#popup3");
+const popup4 = document.querySelector("#popup4");
+const popup5 = document.querySelector("#popup5");
+const popup6 = document.querySelector("#popup6");
+const popup7 = document.querySelector("#popup7");
+const popup8 = document.querySelector("#popup8");
+const popup9 = document.querySelector("#popup9");
+const popup10 = document.querySelector("#popup10");
+const popup11 = document.querySelector("#popup11");
+const popup12 = document.querySelector("#popup12");
+const popup13 = document.querySelector("#popup13");
+const popup14 = document.querySelector("#popup14");
+const popup15 = document.querySelector("#popup15");
+const popup16 = document.querySelector("#popup16");
+const overlay = document.getElementById("overlay");
+
+// Show button
+
+showPopupButton1.addEventListener("click", function () {
+  popup1.style.display = "grid";
+  overlay.style.display = "block";
+});
+showPopupButton2.addEventListener("click", function () {
+  popup2.style.display = "grid";
+  overlay.style.display = "block";
+});
+showPopupButton3.addEventListener("click", function () {
+  popup3.style.display = "grid";
+  overlay.style.display = "block";
+});
+showPopupButton4.addEventListener("click", function () {
+  popup4.style.display = "grid";
+  overlay.style.display = "block";
+});
+showPopupButton5.addEventListener("click", function () {
+  popup5.style.display = "grid";
+  overlay.style.display = "block";
+});
+showPopupButton6.addEventListener("click", function () {
+  popup6.style.display = "grid";
+  overlay.style.display = "block";
+});
+showPopupButton7.addEventListener("click", function () {
+  popup7.style.display = "grid";
+  overlay.style.display = "block";
+});
+showPopupButton8.addEventListener("click", function () {
+  popup8.style.display = "grid";
+  overlay.style.display = "block";
+});
+showPopupButton9.addEventListener("click", function () {
+  popup9.style.display = "grid";
+  overlay.style.display = "block";
+});
+showPopupButton10.addEventListener("click", function () {
+  popup10.style.display = "grid";
+  overlay.style.display = "block";
+});
+showPopupButton11.addEventListener("click", function () {
+  popup11.style.display = "grid";
+  overlay.style.display = "block";
+});
+showPopupButton12.addEventListener("click", function () {
+  popup12.style.display = "grid";
+  overlay.style.display = "block";
+});
+showPopupButton13.addEventListener("click", function () {
+  popup13.style.display = "grid";
+  overlay.style.display = "block";
+});
+showPopupButton14.addEventListener("click", function () {
+  popup14.style.display = "grid";
+  overlay.style.display = "block";
+});
+showPopupButton15.addEventListener("click", function () {
+  popup15.style.display = "grid";
+  overlay.style.display = "block";
+});
+showPopupButton16.addEventListener("click", function () {
+  popup16.style.display = "grid";
+  overlay.style.display = "block";
+});
+
+// Close button
+
+closeButton1.addEventListener("click", function () {
+  popup1.style.display = "none";
+  overlay.style.display = "none";
+});
+closeButton2.addEventListener("click", function () {
+  popup2.style.display = "none";
+  overlay.style.display = "none";
+});
+closeButton3.addEventListener("click", function () {
+  popup3.style.display = "none";
+  overlay.style.display = "none";
+});
+closeButton4.addEventListener("click", function () {
+  popup4.style.display = "none";
+  overlay.style.display = "none";
+});
+closeButton5.addEventListener("click", function () {
+  popup5.style.display = "none";
+  overlay.style.display = "none";
+});
+closeButton6.addEventListener("click", function () {
+  popup6.style.display = "none";
+  overlay.style.display = "none";
+});
+closeButton7.addEventListener("click", function () {
+  popup7.style.display = "none";
+  overlay.style.display = "none";
+});
+closeButton8.addEventListener("click", function () {
+  popup8.style.display = "none";
+  overlay.style.display = "none";
+});
+closeButton9.addEventListener("click", function () {
+  popup9.style.display = "none";
+  overlay.style.display = "none";
+});
+closeButton10.addEventListener("click", function () {
+  popup10.style.display = "none";
+  overlay.style.display = "none";
+});
+closeButton11.addEventListener("click", function () {
+  popup11.style.display = "none";
+  overlay.style.display = "none";
+});
+closeButton12.addEventListener("click", function () {
+  popup12.style.display = "none";
+  overlay.style.display = "none";
+});
+closeButton13.addEventListener("click", function () {
+  popup13.style.display = "none";
+  overlay.style.display = "none";
+});
+closeButton14.addEventListener("click", function () {
+  popup14.style.display = "none";
+  overlay.style.display = "none";
+});
+closeButton15.addEventListener("click", function () {
+  popup15.style.display = "none";
+  overlay.style.display = "none";
+});
+closeButton16.addEventListener("click", function () {
+  popup16.style.display = "none";
+  overlay.style.display = "none";
+});
